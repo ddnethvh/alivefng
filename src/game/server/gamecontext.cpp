07269f9e64c070b7a0be7c649fd026a4c7acad87
scar17off <scar17off@gmail.com>
2025-01-26 21:28:31 +0200

    Refactored laser tuning parameter handling

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -466,22 +466,22 @@ void CGameContext::CheckPureTuning()
 
 void CGameContext::SendTuningParams(int ClientID)
 {
-	CheckPureTuning();
+    CheckPureTuning();
 
-	CMsgPacker Msg(NETMSGTYPE_SV_TUNEPARAMS);
-	int *pParams = (int *)&m_Tuning;
-	for(unsigned i = 0; i < sizeof(m_Tuning)/sizeof(int); i++)
-	{
-			// Update laser reach and bounce num from config
-			if (i == CTuningParams::LASER_REACH)
-					m_Tuning.m_LaserReach = m_Config->m_SvLaserReach;
-			else if (i == CTuningParams::LASER_BOUNCE_NUM)
-					m_Tuning.m_LaserBounceNum = m_Config->m_SvLaserBounceNum;
+    CMsgPacker Msg(NETMSGTYPE_SV_TUNEPARAMS);
+    int *pParams = (int *)&m_Tuning;
+    for(unsigned i = 0; i < sizeof(m_Tuning)/sizeof(int); i++)
+    {
+        // Update laser reach and bounce num from config
+        if (i == m_Tuning.m_LaserReach.id)
+            m_Tuning.m_LaserReach = m_Config->m_SvLaserReach;
+        else if (i == m_Tuning.m_LaserBounceNum.id)
+            m_Tuning.m_LaserBounceNum = m_Config->m_SvLaserBounceNum;
 
-			Msg.AddInt(pParams[i]);
-	}
-	
-	Server()->SendMsg(&Msg, MSGFLAG_VITAL, ClientID);
+        Msg.AddInt(pParams[i]);
+    }
+    
+    Server()->SendMsg(&Msg, MSGFLAG_VITAL, ClientID);
 }
 
 //tune for frozen tees
