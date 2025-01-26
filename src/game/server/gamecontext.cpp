3ae0b3b83852fe68833787d9193e45873216704e
scar17off <scar17off@gmail.com>
2025-01-26 21:26:01 +0200

    Updated laser tuning parameter

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -472,13 +472,13 @@ void CGameContext::SendTuningParams(int ClientID)
 	int *pParams = (int *)&m_Tuning;
 	for(unsigned i = 0; i < sizeof(m_Tuning)/sizeof(int); i++)
 	{
-		// Update laser reach and bounce num from config
-		if (i == m_Tuning.LaserReach.id)
-			m_Tuning.m_LaserReach = m_Config->m_SvLaserReach;
-		else if (i == m_Tuning.LaserBounceNum.id)
-			m_Tuning.m_LaserBounceNum = m_Config->m_SvLaserBounceNum;
+			// Update laser reach and bounce num from config
+			if (i == CTuningParams::LASER_REACH)
+					m_Tuning.m_LaserReach = m_Config->m_SvLaserReach;
+			else if (i == CTuningParams::LASER_BOUNCE_NUM)
+					m_Tuning.m_LaserBounceNum = m_Config->m_SvLaserBounceNum;
 
-		Msg.AddInt(pParams[i]);
+			Msg.AddInt(pParams[i]);
 	}
 	
 	Server()->SendMsg(&Msg, MSGFLAG_VITAL, ClientID);
